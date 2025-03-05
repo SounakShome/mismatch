d39b08fd66e0697175c8ce109f4d7d0307537721
Mohamed Ashif K M <mohamedashif1911@gmail.com>
2025-03-05 22:39:47 +0530

    new footer change

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -98,18 +98,20 @@
                 get back to you as soon as possible.
             </p>
 
-            <div class="space-y-3 text-red-500">
+            <div class="space-y-3 text-red-500 flex flex-col justify-center align-middle">
                 
-                <p>
-                    <i class="fas fa-phone"></i> Sounak : +91 97390 77559
-                </p>
-                <p>
-                    <i class="fas fa-phone"></i> Dev : +91 70168 07505
-                </p>
-                <p>
-                    <i class="fas fa-phone"></i> Ashif : +91 93614 71721
-                </p>
-                
+                <a
+                    href="https://www.instagram.com/ieeerasvitc/"
+                    class="text-red-500  hover:text-red-400 text-xl"
+                    aria-label="Instagram"
+                    ><i class="fab fa-instagram text-4xl"></i> IEEE RAS VITC Instagram</a
+                >
+                <a
+                    href="https://www.linkedin.com/company/ieeerasvitc/posts/?feedView=all"
+                    class="text-red-500  hover:text-red-400 text-xl"
+                    aria-label="LinkedIn"
+                    ><i class="fab fa-linkedin text-4xl"></i>  IEEE RAS VITC LinkedIn</a
+                >
             </div>
         </div>
 
@@ -158,52 +160,4 @@
 </div>
 
 <!-- Footer Section -->
-<footer class="bg-black opacity-70 text-white py-8 px-4 md:px-12 border-t-6">
-    <div class="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-8">
-        <!-- Logo & Description -->
-        <div>
-            <div class="flex items-center space-x-4">
-                <img src="/logo.png" alt="logo" class="h-16 w-16" />
-                <h2 class="text-2xl font-bold">MisMatch'25</h2>
-            </div>
-            <p class="text-gray-400 mt-2">
-                One-Kind-Of Hackathon by the IEEERAS Club of VITC!
-            </p>
-        </div>
 
-        <!-- Quick Links -->
-        <div class="flex flex-col space-y-2">
-            <h3 class="font-semibold text-lg">Quick Links</h3>
-            <a href="/" class="text-gray-400 hover:text-white">Home</a>
-            <a href="/about" class="text-gray-400 hover:text-white">About</a>
-            <a href="/prize" class="text-gray-400 hover:text-white">PrizePool</a
-            >
-            <a href="/tracks" class="text-gray-400 hover:text-white">Tracks</a>
-        </div>
-
-        <!-- Social Media Links -->
-        <div>
-            <h3 class="font-bold text-lg">Follow Us</h3>
-            <div class="flex space-x-8 mt-2">
-                <a
-                    href="https://www.instagram.com/ieeerasvitc/"
-                    class="text-gray-400 hover:text-white text-4xl"
-                    aria-label="Instagram"
-                    ><i class="fab fa-instagram"></i></a
-                >
-                <a
-                    href="https://www.linkedin.com/company/ieeerasvitc/posts/?feedView=all"
-                    class="text-gray-400 hover:text-white text-4xl"
-                    aria-label="LinkedIn"
-                    ><i class="fab fa-linkedin"></i></a
-                >
-            </div>
-        </div>
-    </div>
-
-    <div
-        class="border-t border-gray-700 mt-8 pt-4 text-center text-gray-500 text-sm"
-    >
-        &copy; 2025 MisMatch'25 . All rights reserved.
-    </div>
-</footer>
